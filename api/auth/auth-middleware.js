54327ced65fba8c46f47d1eb7c5508d6cf22f297
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-15 22:03:59 -0600

    MVP done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -40,7 +40,7 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
-  if(role_name === req.decodedJwt) {
+  if(role_name === req.decodedJwt.role_name) {
     next()
   } else {
     next({
